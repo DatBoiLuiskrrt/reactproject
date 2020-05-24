0e9ef1f8d27e624a7295e9c94e66249106b0c8cb
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-05-23 23:52:30 -0500

    added the typical library

--- myreactproject/src/App.js
+++ myreactproject/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import Header from "../src/components/Header";
+import Body from "../src/components/Body";
+import Typical from "react-typical";
 
 function App() {
   return (
     <div>
       <Header/>
+      <h1>hello </h1>
+      <p>Soy Luis y soy{' '}
+        <Typical
+        loop={Infinity}
+        wrapper="b"
+        steps={[
+          'Developer 💻 ',
+          1000,
+          " un Crack 💊",
+          1000,
+        ]}
+        />
+      </p>
+      <Body/>
     </div>
   );
 }
--- myreactproject/src/components/Body.js
+++ myreactproject/src/components/Body.js
@@ -1,10 +1,14 @@
 import React from "react";
-
+import styles from "./Image.module.css";
+import styled from "styled-components";
 function Body() {
-    return
-    <div>
-
+    return (
+    <div className={styles.typewriter}>
+          
+          
     </div>
-};
+    )
+    
+}
 
 export default Body;
