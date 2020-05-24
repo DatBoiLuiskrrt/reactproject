2d71b9c19803a091e1e2098da57cd205d4ffd8ee
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-05-24 13:49:39 -0500

    made the header responsive but still dealing with the container

--- myreactproject/src/components/Header.js
+++ myreactproject/src/components/Header.js
@@ -3,16 +3,16 @@ import styled from 'styled-components'
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GitHubIcon from '@material-ui/icons/GitHub';
 
+
 const Nav = styled.div`
   background-color: #fff;
   border-bottom: 1px solid rgba(0,0,0,.0975);
-  
-  
-  
+  @media (max-width: 500px) {
+    background: linear-gradient(to right, #2193b0, #6dd5ed);
+  }
 `
 const NavHeader = styled.div`
-  border: 1px solid black;
-  
+  // border: 1px solid black;
   display: flex;
   justify-content: space-between;
   max-width: 1010px;
@@ -23,13 +23,24 @@ const NavHeader = styled.div`
   margin: 0 auto;
   @media (max-width: 500px) {
     flex-direction: column;
-
+    // border: 1px dotted black;
+    width: 100%;
   }
 `
 const NavLeft = styled.div`
   width: 33.333%;
   text-align: left;
   font-family: American;
+  @media (max-width: 500px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding-left:0px;
+    margin-left: 0px;
+    
+    width:100%;
+  }
 `
 const NavRight = styled.div`
   width: 33.333%;
@@ -37,13 +48,18 @@ const NavRight = styled.div`
   svg {
     margin-right: 20px;
   }
-  @media (max-width: 500px) {
-    flex-direction: column;
 
-  }
 `
 const MenuLink = styled.a`
 text-decoration: none;
+@media (max-width: 500px) {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  
+}
 `
 
 function Header () {
@@ -56,10 +72,12 @@ function Header () {
         
 
         <NavRight>
-
+          
           <MenuLink href="https://www.linkedin.com/in/datboiluiskrrt/" target="_blank">
             <LinkedInIcon/>
           </MenuLink>
+          
+          
 
           <MenuLink href="https://github.com/DatBoiLuiskrrt"
           target="_blank">
